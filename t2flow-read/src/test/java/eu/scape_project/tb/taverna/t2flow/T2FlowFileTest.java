36e9220d600b656b86c0228dfe05c89026ec9ab7
Sven Schlarb <shsschlarb-scape@yahoo.de>
2012-11-06 10:08:14 +0100

    Code documentation for t2flow reader test added

--- t2flow-read/src/test/java/eu/scape_project/tb/taverna/t2flow/T2FlowFileTest.java
+++ t2flow-read/src/test/java/eu/scape_project/tb/taverna/t2flow/T2FlowFileTest.java
@@ -1,6 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2012 The SCAPE Project Consortium.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * under the License.
  */
 package eu.scape_project.tb.taverna.t2flow;
 
@@ -18,8 +30,10 @@ import uk.org.taverna.scufl2.api.port.InputWorkflowPort;
 import uk.org.taverna.scufl2.api.port.OutputWorkflowPort;
 
 /**
+ * Test class for T2Flow class.
  *
- * @author onbscs
+ * @author Sven Schlarb https://github.com/shsdev
+ * @version 0.1
  */
 public class T2FlowFileTest {
     

d042f7fcdd7f7070151c86f69b23ce102e462b7b
Sven Schlarb <shsschlarb-scape@yahoo.de>
2012-11-12 11:54:12 +0100

    Dependencies cleanup

---
+++ hadoopjobtracker-client/src/main/java/eu/scape_project/tb/hadoopjobtracker/HadoobJobTrackerClient.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.scape_project.tb.hadoopjobtracker;
+
+/**
+ *
+ * @author onbscs
+ */
+public class HadoobJobTrackerClient {
+
+}
